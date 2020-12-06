7bc0b815b4575c6ef38f28d91ad6ad02eacdbf6c
Stan Vlad <vstan02@protonmail.com>
2020-12-06 18:26:43 +0200

    Sync

--- src/gui/gui.c
+++ src/gui/gui.c
@@ -25,7 +25,10 @@
 static void run(Gui* gui, int argc, char** argv) {
     gtk_init(&argc, &argv);
     GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_widget_show(window);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_window_set_title(GTK_WINDOW(window), "xCalc");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+    gtk_widget_show_all(window);
     gtk_main();
 }
 

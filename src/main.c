1cbdbd8dde1a39796f158d4a6d167f7eeee1bfb6
Stan Vlad <63295109+vstan02@users.noreply.github.com>
2023-07-19 21:57:46 +0300

    Updated APP_VERSION and APP_ABOUT

--- src/main.c
+++ src/main.c
@@ -27,11 +27,11 @@
 #include <arithmo.h>
 
 #define APP_NAME "xcalc"
-#define APP_VERSION "0.1.0"
+#define APP_VERSION "1.0.1"
 
 #define APP_ABOUT \
   "xCalc - A fast and simple to use calculator\n" \
-  "Copyright (C) 2020-2021 Stan Vlad <vstan02@protonmail.com>\n\n" \
+  "Copyright (C) 2020-2023 Stan Vlad <vstan02@protonmail.com>\n\n" \
   "This program comes with ABSOLUTELY NO WARRANTY.\n" \
   "This is free software, and you are welcome to redistribute\n" \
   "it under certain conditions.\n" \
